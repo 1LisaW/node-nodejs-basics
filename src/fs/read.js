ac62037bbe947631697a9281191aacfd398f07f3
LisaW <LisaW@ya.ru>
2022-05-31 22:58:25 +0300

    feat: read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.',
+    import.meta.url));
+
 export const read = async () => {
-    // Write your code here 
+    const pathToFile = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    await fs.promises.access(pathToFile)
+        .catch((err) => {throw new Error('FS operation failed')});
+
+    const readStream = fs.createReadStream(pathToFile);
+    readStream.pipe(process.stdout);
 };
+
+read();
