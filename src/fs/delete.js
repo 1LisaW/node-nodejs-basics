f6d96a28d303474df13dae3e2fa4d9ea3ac7b02b
LisaW <LisaW@ya.ru>
2022-05-31 22:30:40 +0300

    feat: delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.',
+    import.meta.url));
+
 export const remove = async () => {
-    // Write your code here 
+    const pathToFile = path.join(__dirname, 'files', 'fileToRemove.txt');
+
+    try {
+        await fs.promises.unlink(pathToFile);
+    } catch {
+        throw new Error('FS operation failed');
+    }
+
 };
+
+remove()
