8b1101d992be3a52c5baab396fe5fbeb2ea5caa8
LisaW <LisaW@ya.ru>
2022-05-31 22:32:57 +0300

    feat: rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,33 @@
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.',
+    import.meta.url));
+
 export const rename = async () => {
-    // Write your code here 
-};
+    const pathToFolder = path.join(__dirname, 'files');
+    const pathToFile = path.join(pathToFolder, 'wrongFilename.txt');
+    const renamedPathToFile = path.join(pathToFolder, 'properFilename.md');
+
+    async function isFileExist(path) {
+        try {
+            await fs.promises.access(path);
+            return true
+        } catch {
+            return false
+        };
+    }
+
+    const isFileRenamed = await isFileExist(renamedPathToFile);
+    if (isFileRenamed) throw new Error('FS operation failed');
+
+    await fs.promises.rename(pathToFile, renamedPathToFile)
+        .catch((error) => {
+            throw new Error('FS operation failed')
+        });
+
+}
+
+
+rename()
