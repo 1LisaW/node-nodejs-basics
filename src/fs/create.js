cf6a23c91aa6e19490fefb4d5b67220774e76fe1
LisaW <LisaW@ya.ru>
2022-05-31 22:30:06 +0300

    feat: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.',
+    import.meta.url));
+
 export const create = async () => {
-    // Write your code here 
+    const message = 'I am fresh and young';
+    const pathToFile = path.join(__dirname, 'files', 'fresh.txt');
+    fs.access(pathToFile, (notExist) => {
+        if (!notExist) throw new Error('FS operation failed')
+    });
+    const writeStream = new fs.createWriteStream(pathToFile, {
+        overwrite: false
+    });
+    await writeStream.write(message);
 };
+create();
