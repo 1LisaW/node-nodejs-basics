d1bec8a9887bb7862d521785159483c70ab7064b
LisaW <LisaW@ya.ru>
2022-05-31 22:29:32 +0300

    feat: copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,42 @@
+import fs from 'fs';
+import path from 'path';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.',
+    import.meta.url));
+
 export const copy = async () => {
-    // Write your code here 
+
+    const folderPath = path.join(__dirname, 'files');
+    const copyFolderPath = path.join(__dirname, 'files_copy');
+
+    async function copyRecursion(folderPath, copyFolderPath) {
+
+        const filesList = await fs.promises.readdir(folderPath, {
+                withFileTypes: true
+            })
+            .catch((notExist) => {
+                if (notExist) throw new Error('FS operation failed');
+            });
+
+        await fs.promises.mkdir(copyFolderPath).catch((err) => {
+            if (err) throw new Error('FS operation failed')
+        });
+        for (let fileName of filesList) {
+            if (fileName.isFile()) {
+                const pathToFileName = path.join(folderPath, fileName.name);
+                const pathToCopyFileName = path.join(copyFolderPath, fileName.name);
+
+                const readStream = fs.createReadStream(pathToFileName);
+                const writeStream = fs.createWriteStream(pathToCopyFileName);
+                readStream.pipe(writeStream);
+            } else {
+                copyRecursion(path.join(folderPath, fileName.name),
+                    path.join(copyFolderPath, fileName.name))
+            }
+        }
+    }
+    copyRecursion(folderPath, copyFolderPath);
 };
+
+copy();
