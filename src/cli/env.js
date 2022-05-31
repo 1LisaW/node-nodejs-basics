b5d72c9d73a9ab4c085b076b5c4fe813f042099c
LisaW <LisaW@ya.ru>
2022-06-01 01:15:37 +0300

    feat: env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,12 @@
 export const parseEnv = () => {
-    // Write your code here 
+    const rssEnvVars = Object.entries(process.env)
+        .reduce((acc,[key, value]) => {
+            if (key.match('RSS')) {
+                acc.push(`${key} = ${value}`)
+            }
+            return acc;
+        },[]);
+    process.stdout.write(rssEnvVars.join('; '))
 };
+parseEnv();
+
