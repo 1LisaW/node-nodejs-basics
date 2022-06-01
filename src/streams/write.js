233ce047f84bf4c521c6eca6809a2b416cdc4eb7
LisaW <LisaW@ya.ru>
2022-06-01 22:59:40 +0300

    feat: write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,17 @@
+import { createWriteStream } from "fs";
+import path from 'path';
+import url from "url";
+
+const __dirname = url.fileURLToPath(new URL('.',
+    import.meta.url));
+
 export const write = async () => {
-    // Write your code here 
+
+    const pathToFile = path.join(__dirname, 'files', 'fileToWrite.txt');
+    const writeStream = createWriteStream(pathToFile);
+    process.stdin.on('data', (data)=>{
+        writeStream.write(data.toString());
+    });
 };
+
+write();
