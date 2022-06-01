4baacffb1024bfcaeb7ecbf31c9f86067dabd80f
LisaW <LisaW@ya.ru>
2022-06-01 22:49:31 +0300

    feat: read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,17 @@
+import { createReadStream } from "fs";
+import path from 'path';
+import url from "url";
+
+const __dirname = url.fileURLToPath(new URL('.',
+    import.meta.url));
+
 export const read = async () => {
-    // Write your code here 
+    
+    const pathToFile = path.join(__dirname, 'files', 'fileToRead.txt');
+    const readStream = createReadStream(pathToFile);
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
 };
+
+read();
